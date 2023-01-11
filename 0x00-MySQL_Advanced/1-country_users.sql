ac78d4ff5efb7d378a9a1b0354cd1d74586cbe56
Tolulope Adeleke <toluadelekesteve@gmail.com>
2023-01-11 18:35:08 +0100

    Add files via upload

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,7 @@
+--table with country field consisting of enumeration of countries: US, CO and TN, never null (= default will be the first element of the enumeration, here US)
+CREATE TABLE IF NOT EXISTS users (
+    id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255) NOT NULL,
+    country ENUM('US', 'CO', 'TN') NOT NULL
+);
