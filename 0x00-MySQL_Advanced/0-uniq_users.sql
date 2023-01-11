50e8814f191bf4cff139b557c813067947412e25
Tolulope <toluadelekesteve@gmail.com>
2023-01-11 16:37:55 +0000

    update

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,5 @@
+-- creates a table users following these requirements
+CREATE TABLE IF NOT EXISTS users (
+    id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255) NOT NULL);
