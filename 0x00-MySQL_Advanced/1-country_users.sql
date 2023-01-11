7578057c2ab42716516a83af94b7a9fa8e6d91b7
Tolulope Adeleke <toluadelekesteve@gmail.com>
2023-01-11 18:42:08 +0100

    Update 1-country_users.sql

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -2,7 +2,7 @@
 -- countries: US, CO and TN, never null (= default will be
 -- the first element of the enumeration, here US)
 CREATE TABLE IF NOT EXISTS users (
-    id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255) NOT NULL,
     country ENUM('US', 'CO', 'TN') NOT NULL
